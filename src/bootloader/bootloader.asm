744a70ea6e2a5c0bfb631b7875b7caf5845134ca
Thomas Bro Falkenberg <tbf@mit.edu>
2021-07-16 00:20:13 +0200

    Initial 'Hello World' bootloader

---
+++ src/bootloader/bootloader.asm
@@ -0,0 +1,21 @@
+;
+;   Bootloader for OS system
+;
+
+mov ah, 0x0e            ; BIOS print routine
+
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $
+
+times 510-($-$$) db 0
+dw 0xaa55
