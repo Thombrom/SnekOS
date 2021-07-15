7381ba2e182e595ea6116edad841498edcb82333
Thomas Bro Falkenberg <tbf@mit.edu>
2021-07-16 00:20:29 +0200

    Compilation script for bootloader

--- bin/internal_compile.sh
+++ bin/internal_compile.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+# Create build directory
+BUILD_DIR=build
+OBJ_DIR=obj
+
+if [[ -d $BUILD_DIR ]]; then
+    rm -r $BUILD_DIR
+fi
+mkdir $BUILD_DIR
+mkdir $BUILD_DIR/$OBJ_DIR
+
+# Compile bootloader with NASM
+BOOTLOADER=src/bootloader/bootloader.asm
+
+nasm $BOOTLOADER -f bin -o $BUILD_DIR/$OBJ_DIR/bootloader.bin
+
+
+# Create ELF
+cp $BUILD_DIR/$OBJ_DIR/bootloader.bin $BUILD_DIR/image.elf
+
 echo "Internal compilation successful"
