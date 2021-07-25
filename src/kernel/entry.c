4fa5266dee54ea318975e08749bef38cafe41f0c
Thomas Bro Falkenberg <thom821f2@gmail.com>
2021-07-25 17:44:50 +0200

    Temporary entry file showing IDT setup and hooking interrupt functions

--- src/kernel/entry.c
+++ src/kernel/entry.c
@@ -3,9 +3,12 @@
 
 #include "idt.h"
 
+void keyboard_interrupt_handler(struct interrupt_frame _frame);
+
 // Kernel Entrypoint
 void _start() {
-    idt_load();
+    idt_init();
+    register_interrupt_handler(&keyboard_interrupt_handler, 0x09);
 
     screen_clear_f(FG_WHITE | BG_BLACK);
     screen_set_cursor_position(10, 3);
@@ -26,4 +29,27 @@ void _start() {
     }
 
     while (1);
+}
+
+void keyboard_interrupt_handler(struct interrupt_frame _frame)
+{
+    uint8_t scan_code;
+    in_byte(0x60, &scan_code);
+
+    uint8_t digits[3] = { 0, 0, 0 };
+    uint8_t* scan_code_str = "000";
+
+    digits[0] = (scan_code - scan_code % 100) / 100;
+    digits[1] = (scan_code - digits[0] * 100 - scan_code % 10) / 10;
+    digits[2] = scan_code % 10;
+
+    scan_code_str[0] = 48 + digits[0];
+    scan_code_str[1] = 48 + digits[1];
+    scan_code_str[2] = 48 + digits[2];
+
+    screen_set_cursor_position(0, 0);
+    screen_print_string(scan_code_str);
+
+    out_byte(0x20, 0x20);
+    out_byte(0xa0, 0x20);
 }
