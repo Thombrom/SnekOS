c6f473133dca776e26b345f49d515db4ed003971
Thomas Bro Falkenberg <thom821f2@gmail.com>
2021-07-29 20:54:13 +0200

    Added dedicated functions for disable/enable PIC interrupts

--- src/kernel/pic.c
+++ src/kernel/pic.c
@@ -8,3 +8,25 @@ void pic_acknowledge(uint8_t _interrupt)
 
     out_byte(PIC_MASTER_COMMAND_PORT, PIC_ACKNOWLEDGE);
 }
+
+void pic_enable(uint8_t _vector)
+{
+    uint8_t offset   = _vector > 7 ? _vector - 8 : _vector;
+    uint8_t data_port = _vector > 7 ? PIC_SLAVE_DATA_PORT : PIC_MASTER_DATA_PORT;
+
+    // Read current configuration and change position
+    uint8_t flags;
+    in_byte(data_port, &flags);
+    out_byte(data_port, flags | (1 << offset));
+}
+
+void pic_disable(uint8_t _vector)
+{
+    uint8_t offset   = _vector > 7 ? _vector - 8 : _vector;
+    uint8_t data_port = _vector > 7 ? PIC_SLAVE_DATA_PORT : PIC_MASTER_DATA_PORT;
+
+    // Read current configuration and change position
+    uint8_t flags;
+    in_byte(data_port, &flags);
+    out_byte(data_port, flags & ~(1 << offset));
+}
--- src/kernel/pic.h
+++ src/kernel/pic.h
@@ -6,7 +6,7 @@
 #define PIC_MASTER_COMMAND_PORT     0x20
 #define PIC_MASTER_DATA_PORT        0x21
 #define PIC_SLAVE_COMMAND_PORT      0xa0
-#define PIC_SLAVE_DATA_OPRT         0xa1
+#define PIC_SLAVE_DATA_PORT         0xa1
 
 #define PIC_MASTER_START_INTERRUPT  0x08
 #define PIC_MASTER_END_INTERRUPT    PIC_MASTER_START_INTERRUPT + 7
@@ -15,4 +15,31 @@
 
 #define PIC_ACKNOWLEDGE             0x20
 
+#define ICW1_INIT       0x10
+#define ICW1_ICW4       0x01
+#define ICW4_8086       0x01
+
+/*
+ *  pic_acknowledge
+ *
+ *  For sending acknowledge message
+ *  to PIC upon interrupt
+ */
+
 void pic_acknowledge(uint8_t _interrupt);
+
+/*
+ *  pic_enable
+ *
+ *  Enables a PIC vector for interrupts
+ */
+
+void pic_enable(uint8_t _vector);
+
+/*
+ *  pic_disable
+ *
+ *  Disables a PIC vector for interrupts
+ */
+
+void pic_disable(uint8_t _vector);
